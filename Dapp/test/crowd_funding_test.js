3e016ee49ae67ae1dc3169ac3d8b9262d46241fe
Praveen Kumar Pandurangan <praveenkpandu@gmail.com>
2021-08-15 23:08:39 +0100

    More test cases added and test

--- Dapp/test/crowd_funding_test.js
+++ Dapp/test/crowd_funding_test.js
@@ -2,7 +2,7 @@ let CrowdFunding = artifacts.require('./TestCrowdFunding')
 const BigNumber = require('BigNumber.js')
 
 
-contract('CrowdFunding', function(accounts)) {
+contract('CrowdFunding', function(accounts) {
     let contract;
     let contractOwner = accounts[0];
     let beneficiary = accounts[1];
@@ -15,11 +15,51 @@ contract('CrowdFunding', function(accounts)) {
     const PAID_OUT_STATE = 3;
 
     beforeEach(async function() {
-        contract = await CrowdFunding.new('Funding', 1, 5, beneficiary, {from: contractCreator, gas: 2000000})
+        contract = await CrowdFunding.new('Funding', 1, 5, beneficiary, {from: contractOwner, gas: 2000000})
     })
 
     it('contract is initialized', async function() {
         let contractName = await contract.name.call()
         expect(contractName).to.equal('Funding');
     })
-}
+
+    it('funds are contributed', async function() {
+        await contract.contribute({value: ONE_ETH, from: contractOwner});
+
+        let contributed = await contract.amounts.call(contractOwner);
+        expect(ONE_ETH.isEqualTo(contributed)).to.equal(true);
+    })
+
+    it('Deadline passed. Cannot contribute', async function() {
+        await contract.contribute({value: ONE_ETH, from: contractOwner});
+
+        let contributed = await contract.amounts.call(contractOwner);
+        expect(ONE_ETH.isEqualTo(contributed)).to.equal(true);
+    })
+
+    it('Paidout collected money', async function() {
+        await contract.contribute({value: ONE_ETH, from: contractOwner});
+        await contract.setCurrentTime(601);
+        await contract.finishCrowdFunding();
+
+        let initAmount = await web3.eth.getBalance(beneficiary);
+        await contract.collect({from: contractOwner});
+
+        let newBalance = await web3.eth.getBalance(beneficiary);
+        let difference = newBalance - initAmount;
+        expect(ONE_ETH.isEqualTo(difference)).to.equal(true);
+
+        let fundingState = await contract.state.call()
+        expect(fundingState.valueOf().toNumber()).to.equal(PAID_OUT_STATE);
+    })
+
+    it('withdraw funds from the contract', async function() {
+        await contract.contribute({value: ONE_ETH - 100, from: contractOwner});
+        await contract.setCurrentTime(601);
+        await contract.finishCrowdFunding();
+
+        await contract.withdraw({from: contractOwner});
+        let amount = await contract.amounts.call(contractOwner);
+        expect(amount.toNumber()).to.equal(0);
+    });
+});
